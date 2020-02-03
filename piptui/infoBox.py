51c6a9018b89b7e86558ad2d1173c201c27daced
nitanmarcel <nitan.marcel@protonmail.com>
2020-02-03 22:39:16 +0200

    Fix index out of range when no package is found

--- piptui/infoBox.py
+++ piptui/infoBox.py
@@ -15,16 +15,20 @@ class InfoBox(BoxTitle):
         self.value = 'Loading package description!'
         self.update()
         current_selection = self.parent.parentApp.MainForm.PkgBoxObj.value
-        pkg = self.parent.parentApp.MainForm.PkgBoxObj.values[current_selection].split()[
-            0]
-        request = requests.get('https://pypi.org/pypi/{}/json'.format(pkg))
-        if request.status_code == 200:
-            data = request.json()
-            info = data.get('info')
-            description = info.get('description')
-            summary = info.get('summary')
-            self.value = description or summary or "Package description unavailable"
+        pkg_val = self.parent.parentApp.MainForm.PkgBoxObj.values
+        if len(pkg_val) > 0:
+            pkg = self.parent.parentApp.MainForm.PkgBoxObj.values[current_selection].split()[
+                0]
+            request = requests.get('https://pypi.org/pypi/{}/json'.format(pkg))
+            if request.status_code == 200:
+                data = request.json()
+                info = data.get('info')
+                description = info.get('description')
+                summary = info.get('summary')
+                self.value = description or summary or "Package description unavailable"
+            else:
+                self.value = "Couldn't find this package on pypi.org! ERROR: " + \
+                    str(request.status_code)
         else:
-            self.value = "Couldn't find this package on pypi.org! ERROR: " + \
-                str(request.status_code)
+            self.value = "No packages found"
         self.update()
